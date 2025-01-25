44e7bfed769cf3d94fe0f06ad8d3f137c7491553
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-01-25 22:24:25 +0000

    Fix renderWithQueryClient for testing
    For testing, retries should be disabled since they can cause tests
    to timeout. Moreover, since the React Query client stores state, a
    new query client should be created for each call.

--- lingetic-nextjs-frontend/utilities/testing-helpers.tsx
+++ lingetic-nextjs-frontend/utilities/testing-helpers.tsx
@@ -1,9 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 
-const queryClient = new QueryClient();
-
 export const renderWithQueryClient = (component: React.ReactElement) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
   return render(
     <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
   );
