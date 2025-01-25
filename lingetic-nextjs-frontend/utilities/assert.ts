2485daf11011c044d73d73322d94e90241914ee5
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-01-25 17:46:59 +0000

    Add utility functions for logging and assertions
    - Create a logger utility with Sentry integration for different log levels
    - Implement an assert function to handle and log fatal assertions
    
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

---
+++ lingetic-nextjs-frontend/utilities/assert.ts
@@ -0,0 +1,10 @@
+import log from './logger';
+
+export default function assert(isAssertionTrue: boolean, assertionDescription: string) {
+  if (isAssertionTrue) {
+    return;
+  }
+
+
+  log(assertionDescription, 'fatal');
+}
