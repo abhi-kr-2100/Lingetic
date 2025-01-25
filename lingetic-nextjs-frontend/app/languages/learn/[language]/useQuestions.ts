2f0cacfc0f82ba21882983c0f59fd49f94bc2b07
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-01-25 20:31:10 +0000

    Fetch questions from the API instead of the mock

--- lingetic-nextjs-frontend/app/languages/learn/[language]/useQuestions.ts
+++ lingetic-nextjs-frontend/app/languages/learn/[language]/useQuestions.ts
@@ -1,17 +1,76 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-interface UseQuestionsParams {
-  questions: Array<{
-    id: string;
-    type: "FillInTheBlanks";
-    text: string;
-    hint: string;
-  }>;
-  onFinish: () => void;
+interface Question {
+  id: string;
+  type: "FillInTheBlanks";
+  text: string;
+  hint: string;
 }
 
-export function useQuestions({ questions, onFinish }: UseQuestionsParams) {
+interface UseQuestionsParams {
+  onFinish: () => void;
+  language: string;
+}
+
+interface LoadingState {
+  isLoading: true;
+  isError: false;
+}
+
+interface ErrorState {
+  isLoading: false;
+  isError: true;
+}
+
+interface SuccessState {
+  isLoading: false;
+  isError: false;
+  currentQuestion: Question;
+  isLastQuestion: boolean;
+  onNext: () => void;
+}
+
+type UseQuestionsResult = LoadingState | ErrorState | SuccessState;
+
+export default function useQuestions({
+  onFinish,
+  language,
+}: UseQuestionsParams): UseQuestionsResult {
+  const {
+    data: questions = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["questions", language],
+    queryFn: () => fetchQuestions(language),
+
+    // The same questions should not be displayed on different renders
+    // of the component. This is because the questions that Lingetic thinks
+    // the user should attempt may change any time.
+    refetchOnMount: "always",
+
+    // If user navigates away from the page in between a run of a playlist,
+    // the playlist shouldn't refresh and change the questions the user was
+    // working with.
+    refetchOnWindowFocus: false,
+  });
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  if (isLoading) {
+    return {
+      isLoading: true,
+      isError: false,
+    };
+  }
+
+  if (isError) {
+    return {
+      isLoading: false,
+      isError: true,
+    };
+  }
 
   const onNext = () => {
     if (currentQuestionIndex < questions.length - 1) {
@@ -22,8 +81,22 @@ export function useQuestions({ questions, onFinish }: UseQuestionsParams) {
   };
 
   return {
+    isLoading: false,
+    isError: false,
     currentQuestion: questions[currentQuestionIndex],
     isLastQuestion: currentQuestionIndex === questions.length - 1,
     onNext,
   };
-} 
+}
+
+const fetchQuestions = async (language: string): Promise<Question[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/questions?language=${language}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch questions");
+  }
+
+  return res.json();
+};
