07090290e1337acf616bc6030fb11d175e30727f
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-01-25 20:04:49 +0000

    Use a central Provider component in Root Layout
    Earlier, providers were used where required. This is not always
    possible, and leads to duplicated code.

--- lingetic-nextjs-frontend/app/layout.tsx
+++ lingetic-nextjs-frontend/app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import Navbar from './components/NavBar';
+import "./globals.css";
+import type { Metadata } from "next";
+import Navbar from "./components/NavBar";
+import Providers from "./providers";
 
 export const metadata: Metadata = {
-  title: 'Lingetic',
-  description: 'AI-Powered Language Learning for Real-World Conversations',
+  title: "Lingetic",
+  description: "AI-Powered Language Learning for Real-World Conversations",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
