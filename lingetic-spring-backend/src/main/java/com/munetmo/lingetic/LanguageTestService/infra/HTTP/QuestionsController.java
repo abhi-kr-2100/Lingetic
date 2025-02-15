96ce10b799ef4ae4422ce13f5e982cf42b87bab7
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-02-15 17:12:53 +0530

    Refactor

--- lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageTestService/infra/HTTP/QuestionsController.java
+++ lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageTestService/infra/HTTP/QuestionsController.java
@@ -7,12 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.munetmo.lingetic.LanguageTestService.DTOs.Question.QuestionDTO;
-
 import com.munetmo.lingetic.LanguageTestService.UseCases.AttemptQuestionUseCase;
 import com.munetmo.lingetic.LanguageTestService.UseCases.TakeRegularTestUseCase;
-
-import java.util.List;
 
 @CrossOrigin(origins = "${spring.web.cors.allowed-origins}")
 @RestController
