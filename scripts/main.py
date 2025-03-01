0a730229c4670926213ceeff32440ca1be4b974e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-03-01 15:46:23 +0530

    Modify main.py to execute given scripts

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
+from sys import argv, exit, stderr
+from typing import Iterable
 
-def main():
-    print("Hello, world!")
+import scripts
+
+
+def main(argv: Iterable[str]):
+    if len(argv) < 1:
+        print("Usage: main.py script arguments...", file=stderr)
+        exit(1)
+
+    script_name = argv[0]
+    script_args = argv[1:]
+
+    if not hasattr(scripts, script_name):
+        print(f"Error: script '{script_name}' not found", file=stderr)
+        exit(1)
+
+    script_module = getattr(scripts, script_name)
+    script_module.main(*script_args)
 
 
 if __name__ == "__main__":
-    main()
+    main(argv[1:])
