3092f2a8adbef4a0661424bdbaf4949887c8afe0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-03-03 22:10:40 +0530

    Separate AuthButton's child components

---
+++ lingetic-nextjs-frontend/app/components/NavBar/AuthButton/LoadingButton.tsx
@@ -0,0 +1,7 @@
+export default function LoadingButton() {
+  return (
+    <button className="bg-skin-button-primary text-skin-inverted px-4 py-2 rounded-md opacity-70 cursor-wait">
+      Loading...
+    </button>
+  );
+}
