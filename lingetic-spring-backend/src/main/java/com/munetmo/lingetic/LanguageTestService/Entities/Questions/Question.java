3aaff4f85e936f0d92f3832e37808e928a4c3d00
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-03-09 14:37:01 +0530

    Add methods to work with question specific data
    This will make reading and writing to DBs that
    don't support object-oriented inheritance simpler.

--- lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageTestService/Entities/Questions/Question.java
+++ lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageTestService/Entities/Questions/Question.java
@@ -4,11 +4,22 @@ import com.munetmo.lingetic.LanguageTestService.DTOs.Attempt.AttemptRequests.Att
 import com.munetmo.lingetic.LanguageTestService.DTOs.Attempt.AttemptResponses.AttemptResponse;
 import com.munetmo.lingetic.LanguageTestService.Entities.Language;
 
+import java.util.Map;
+
 public sealed interface Question permits FillInTheBlanksQuestion {
     String getID();
     QuestionType getQuestionType();
     Language getLanguage();
-    AttemptResponse assessAttempt(AttemptRequest request);
     int getDifficulty();
     String getQuestionListID();
+
+    Map<String, Object> getQuestionTypeSpecificData();
+    static Question createFromQuestionTypeSpecificData(String id, Language language, int difficulty, String questionListId, QuestionType questionType, Map<String, Object> data) {
+        return switch (questionType) {
+            case FillInTheBlanks -> FillInTheBlanksQuestion.createFromQuestionTypeSpecificData(id, language, difficulty, questionListId, data);
+            default -> throw new IllegalArgumentException("Unsupported question type");
+        };
+    }
+
+    AttemptResponse assessAttempt(AttemptRequest request);
 }
