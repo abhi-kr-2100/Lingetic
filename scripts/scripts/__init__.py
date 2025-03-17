7cfe2de7fa13e54f41bc07073e169dde32b7646e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-03-17 19:51:25 +0530

    Create a script to generate fill in the blanks

--- scripts/scripts/__init__.py
+++ scripts/scripts/__init__.py
@@ -2,5 +2,12 @@ from . import hello
 from . import tatoeba
 from . import sort_sentences
 from . import filter_sentences
+from . import sentence_to_fill_in_the_blanks
 
-__all__ = ["hello", "tatoeba", "sort_sentences", "filter_sentences"]
+__all__ = [
+    "hello",
+    "tatoeba",
+    "sort_sentences",
+    "filter_sentences",
+    "sentence_to_fill_in_the_blanks",
+]
