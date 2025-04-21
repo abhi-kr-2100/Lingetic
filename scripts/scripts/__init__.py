a242ad4285d62114b724359a7a034c5b2c6bdbec
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-04-21 22:48:41 +0530

    Remove useless scripts

--- scripts/scripts/__init__.py
+++ scripts/scripts/__init__.py
@@ -1,23 +1,15 @@
-from . import hello
 from . import tatoeba
 from . import sort
-from . import filter_sentences
 from . import fill_in_the_blanks
 from . import insert_into_db
-from . import images_to_sentence_jsons
-from . import pdf_to_images
 from . import tts
 from . import upload_to_r2
 
 __all__ = [
-    "hello",
     "tatoeba",
     "sort",
-    "filter_sentences",
     "fill_in_the_blanks",
     "insert_into_db",
-    "images_to_sentence_jsons",
-    "pdf_to_images",
     "tts",
     "upload_to_r2",
 ]
