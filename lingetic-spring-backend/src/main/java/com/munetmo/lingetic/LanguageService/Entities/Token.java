2dd61671ce2c59168b95b2243ce81228fc266e41
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-04-23 21:47:53 +0530

    Add Tokens

--- lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageService/Entities/Token.java
+++ lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageService/Entities/Token.java
@@ -1,4 +1,9 @@
 package com.munetmo.lingetic.LanguageService.Entities;
 
-public class Token {
+public record Token(TokenType type, String value) {
+    public Token {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("value cannot be blank");
+        }
+    }
 }
