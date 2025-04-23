989b7daf4196d855e2ea8f07845c8ca90baad0f5
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-04-24 00:03:28 +0530

    Add endpoint to tokenize text

--- lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageService/infra/HTTP/LanguageServiceController.java
+++ lingetic-spring-backend/src/main/java/com/munetmo/lingetic/LanguageService/infra/HTTP/LanguageServiceController.java
@@ -1,16 +1,24 @@
 package com.munetmo.lingetic.LanguageService.infra.HTTP;
 
+import com.munetmo.lingetic.LanguageService.Entities.Language;
+import com.munetmo.lingetic.LanguageService.Entities.LanguageModels.LanguageModel;
+import com.munetmo.lingetic.LanguageService.Entities.Token;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/language-service")
 @ConditionalOnProperty(name = "app.environment", havingValue = "development")
 public class LanguageServiceController {
-    @GetMapping
-    public String hello() {
-        return "Hello";
+    @GetMapping("/tokenize")
+    public ResponseEntity<List<Token>> tokenize(
+            @RequestParam("language") Language language,
+            @RequestParam("sentence") String sentence) {
+        var model = LanguageModel.getLanguageModel(language);
+        var tokens = model.tokenize(sentence);
+        return ResponseEntity.ok(tokens);
     }
 }
