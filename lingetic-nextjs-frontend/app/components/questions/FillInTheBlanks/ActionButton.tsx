c9f0e2fe22cee252b43f6845f9d1f5187383c2b3
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-05-03 22:18:17 +0530

    UI changes

---
+++ lingetic-nextjs-frontend/app/components/questions/FillInTheBlanks/ActionButton.tsx
@@ -0,0 +1,44 @@
+import Speaker from "./Speaker";
+
+import type { ReactNode } from "react";
+import type { FillInTheBlanksQuestion } from "@/utilities/api-types";
+
+interface ActionButtonProps {
+  isChecked: boolean;
+  isChecking: boolean;
+  onCheck: () => void;
+  NextButton: ReactNode;
+  question: FillInTheBlanksQuestion;
+  value: string;
+}
+
+export default function ActionButton({
+  isChecked,
+  isChecking,
+  onCheck,
+  NextButton,
+  question,
+  value,
+}: ActionButtonProps) {
+  return (
+    <div className="flex justify-end items-center gap-3">
+      <Speaker question={question} autoplay />
+      {isChecked ? (
+        NextButton
+      ) : (
+        <button
+          type="button"
+          disabled={isChecking}
+          onClick={onCheck}
+          className="bg-skin-button-primary text-white px-6 py-3 rounded-lg font-medium hover:bg-skin-button-primary-hover transition-colors flex items-center"
+        >
+          {isChecking
+            ? "Checking..."
+            : value.trim().length === 0
+            ? "Skip"
+            : "Check"}
+        </button>
+      )}
+    </div>
+  );
+}
